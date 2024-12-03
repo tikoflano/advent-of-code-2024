6bd9a2795d1ea10b4683c7da75993ab567f5f622
Alvaro Flaño <tikoflano@gmail.com>
2024-12-03 14:42:05 +0000

    Add day 3 - problem 1 solution

---
+++ problems/2024_03_1.go
@@ -0,0 +1,29 @@
+package problems
+
+import (
+	"regexp"
+	"strconv"
+)
+
+func init() {
+	solutions["2024_03_1"] = year2024Day3Problem1
+}
+
+// https://adventofcode.com/2024/day/3
+func year2024Day3Problem1(input []string) string {
+	resp := 0
+	re := regexp.MustCompile(`mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+
+	for _, line := range input {
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+
+			resp += num1 * num2
+		}
+	}
+
+	return strconv.Itoa(resp)
+}
